b7a15b1d9d6f7ed228730c8799f14ae0d35bc893
Mdubois2705 <m.dubois2705@gmail.com>
2026-07-02 09:54:24 +0200

    add page account + dossier auth + modif des pages en general

--- router/allroutes.js
+++ router/allroutes.js
@@ -4,8 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html")
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+    new Route("/editpassword", "Modifier mon mot de passe", "/pages/auth/editpassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
